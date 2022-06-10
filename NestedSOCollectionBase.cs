3d39f782ba5130a052dfac95f231f5fbb8a3cd33
DESKTOP-3T4A7P1\Ramses <ik_ben_ramses@hotmail.com>
2022-06-10 11:09:07 +0200

    Made it so a NestedSOCollection is also a IReadOnlyList and added lookup / query methods

--- NestedSOCollectionBase.cs
+++ NestedSOCollectionBase.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 namespace NestedSO
 {
-	public abstract class NestedSOCollectionBase<NestedSOItemT> : ScriptableObject, INestedSOCollection
+	public abstract class NestedSOCollectionBase<NestedSOItemT> : ScriptableObject, INestedSOCollection, IReadOnlyList<NestedSOItemT>
 	where NestedSOItemT : ScriptableObject
 	{
 		#region Editor Variables
@@ -12,11 +15,17 @@ namespace NestedSO
 
 		#endregion
 
+		#region Properties
+
+		public int Count => _nestedSOItems.Count;
+
+		#endregion
+
 		#region Public Methods
 
 		public void AddAsset(ScriptableObject item)
 		{
-			if (item is NestedSOItemT castedItem)
+			if(item is NestedSOItemT castedItem)
 			{
 				_nestedSOItems.Add(castedItem);
 			}
@@ -24,14 +33,107 @@ namespace NestedSO
 
 		public void RemoveAsset(ScriptableObject item)
 		{
-			if (item is NestedSOItemT castedItem)
+			if(item is NestedSOItemT castedItem)
 			{
 				_nestedSOItems.Remove(castedItem);
 			}
 		}
 
+		public void ForEach(Action<NestedSOItemT> action)
+		{
+			for(int i = 0, c = _nestedSOItems.Count; i < c; i++)
+			{
+				action(_nestedSOItems[i]);
+			}
+		}
+
+		public void ForEachReversed(Action<NestedSOItemT> action)
+		{
+			for(int i = _nestedSOItems.Count - 1; i >= 0; i--)
+			{
+				action(_nestedSOItems[i]);
+			}
+		}
+
+		public bool TryGetItem(Predicate<NestedSOItemT> predicate, out NestedSOItemT item)
+		{
+			for(int i = 0, c = _nestedSOItems.Count; i < c; i++)
+			{
+				item = _nestedSOItems[i];
+				if(predicate(item))
+				{
+					return true;
+				}
+			}
+			item = null;
+			return false;
+		}
+
+		public bool TryGetItem<T>(out T item) where T : NestedSOItemT
+		{
+			for(int i = 0, c = _nestedSOItems.Count; i < c; i++)
+			{
+				NestedSOItemT baseItem = _nestedSOItems[i];
+				if(baseItem is T castedItem)
+				{
+					item = castedItem;
+					return true;
+				}
+			}
+
+			item = null;
+			return false;
+		}
+
+		public bool TryGetItem<T>(Predicate<T> predicate, out T item) where T : NestedSOItemT
+		{
+			for(int i = 0, c = _nestedSOItems.Count; i < c; i++)
+			{
+				NestedSOItemT baseItem = _nestedSOItems[i];
+				if(baseItem is T castedItem && predicate(castedItem))
+				{
+					item = castedItem;
+					return true;
+				}
+			}
+
+			item = null;
+			return false;
+		}
+
+		public List<T> GetItems<T>() where T : NestedSOItemT
+		{
+			List<T> castedItems = new List<T>();
+			for(int i = 0, c = _nestedSOItems.Count; i < c; i++)
+			{
+				var item = _nestedSOItems[i];
+				if(item is T castedItem)
+				{
+					castedItems.Add(castedItem);
+				}
+			}
+			return castedItems;
+		}
+
+		public List<T> GetItems<T>(Predicate<T> predicate) where T : NestedSOItemT
+		{
+			List<T> castedItems = new List<T>();
+			for(int i = 0, c = _nestedSOItems.Count; i < c; i++)
+			{
+				var item = _nestedSOItems[i];
+				if(item is T castedItem && predicate(castedItem))
+				{
+					castedItems.Add(castedItem);
+				}
+			}
+			return castedItems;
+		}
+
 		public IReadOnlyList<ScriptableObject> GetRawItems() => _nestedSOItems;
 		public IReadOnlyList<NestedSOItemT> GetItems() => _nestedSOItems;
+		public NestedSOItemT this[int index] => _nestedSOItems[index];
+		public IEnumerator<NestedSOItemT> GetEnumerator() => _nestedSOItems.GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => _nestedSOItems.GetEnumerator();
 
 		#endregion
 	}
